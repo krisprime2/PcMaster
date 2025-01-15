f218511651652eec63afc7daa2928912bb8953d1
Kristoffer Götz <kr121goe@htwg-konstanz.de>
2025-01-15 13:23:14 +0100

    changes for prod.js

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -344,7 +344,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+     trustProxy: true,
 
   },
 
@@ -359,7 +359,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  // port: 80,
+   port: 10000,
 
 
 
--- backend/tasks/register/prod.js
+++ backend/tasks/register/prod.js
@@ -13,14 +13,15 @@
  */
 module.exports = function(grunt) {
   grunt.registerTask('prod', [
-    'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
+    //'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
     'compileAssets',
-    'babel',         //« Remove this to skip transpilation in production (not recommended)
-    'concat',
-    'uglify',
-    'cssmin',
-    'sails-linker:prodJs',
-    'sails-linker:prodStyles',
+    'linkAssets',
+    //'babel',         //« Remove this to skip transpilation in production (not recommended)
+    //'concat',
+    //'uglify',
+    //'cssmin',
+    //'sails-linker:prodJs',
+    //'sails-linker:prodStyles',
   ]);
 };
 
